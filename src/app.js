026a87464c2d00e1ddca487e1bb8d7bf18944493
franciscoespinosasuarez <fj.espinosa.suarez@gmail.com>
2022-05-02 19:53:27 +0000

    ejercicio completado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let site = [".com", ".es", ".net"];
+  let frase = "";
+  let dominio = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        //console.log(pronoun[i] + adj[a] + noun[b]);
+        for (let c = 0; c < site.length; c++) {
+          frase = pronoun[i] + adj[a] + noun[b] + site[c];
+          dominio.push(frase);
+        }
+      }
+    }
+  }
+
+  let listaDominio = document.querySelector("#lista-dominios");
+
+  for (let i = 0; i < dominio.length; i++) {
+    let p = document.createElement("p");
+    p.textContent = dominio[i];
+    listaDominio.appendChild(p);
+  }
 };
